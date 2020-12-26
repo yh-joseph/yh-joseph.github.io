93fa8cb390a6b7e149edd855bf6f378407159fc8
Valani <38432812+Valani@users.noreply.github.com>
2020-12-26 10:30:49 +0200

    add content on main page

--- js/script.js
+++ js/script.js
@@ -4,14 +4,7 @@ $(document).ready(function(){
 
 
  
-    $('.rev_slider').slick({
- slidesToShow: 3,
-      infinite: true,
 
-  autoplay: true,
-  autoplaySpeed: 2000,             
-
-});
     $('.header__adaptiv').on('click',function(e){
         $('.menu__hidden-2').toggleClass('menu__visible')
          $('.header__logo').toggleClass('logofix')
